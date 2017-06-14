86ed2330a56ec2737e3a7f91bf69b64bc97a71fb
spragala <spragala@yahoo.com>
2017-06-14 09:31:59 -0700

    HTML template for displaying data

--- src/App.js
+++ src/App.js
@@ -22,6 +22,28 @@ class App extends Component {
     return (
       <div className="App">
           <Header />
+          <div className="container">
+        <div className="row" data-id="${restaurants._id}">
+          <div className="col-xs-1">
+            <img src="#" className="images" />
+          </div>
+          <div className="col-xs-4">
+            <h2 className="restaurant-name">Restaurant Name</h2>
+            <p className="rating">Rating stars out of 5</p>
+            <p className="address">Address</p>
+            <p className="text-muted"><em>Restaurant Type</em></p>
+          </div>
+          <div className="col-xs-4">
+            <p><strong>Notes:</strong></p>
+            <p className="notes"></p>
+          </div>
+          <div className="col-xs-2">
+            <button type="button" name="button" className="deleteBtn btn btn-default" data-id="${restaurants._id}">
+              <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+            </button>
+          </div>
+        </div>
+      </div>
           <Footer />
       </div>
     );
