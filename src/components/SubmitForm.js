c0625223d60f186d813ef68f7473317900c4e280
spragala <spragala@yahoo.com>
2017-06-14 15:52:25 -0700

    form submits inputed data for each field

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 class SubmitForm extends Component {
-  constructor(){
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      restaurant: ''
+      restaurant: '',
+      city: ''
     }
+    this.onInputChange = this.onInputChange.bind(this)
   }
+
+  onInputChange(event){
+    console.log('create todo input changed')
+    const target  = event.target;
+    const value   = target.value;
+    const name    = target.name;
+
+    this.setState({
+      [name]: value
+    })
+    }
+
 
   onFormSubmit(event){
   console.log('form submitted')
+  console.log(this.state.restaurant)
+  console.log(this.state.city);
   event.preventDefault()
+
   }
 
   render() {
@@ -19,11 +36,21 @@ class SubmitForm extends Component {
         <div className="row">
           <div className=" col-xs-6 col-md-4 col-xs-offset-1">
             <label htmlFor="restaurant">Restaurant</label>
-            <input type="text" className="form-control" name="name" placeholder="Restaurant full name" />
+            <input type="text"
+                   className="form-control"
+                   name="restaurant"
+                   placeholder="Restaurant full name"
+                   onChange={ this.onInputChange }
+                   value={ this.state.restaurant }/>
           </div>
           <div className="col-xs-6 col-md-4 ">
             <label htmlFor="city">City</label>
-            <input type="text" className="form-control" name="address" placeholder="City e.g. Oakland" />
+            <input type="text"
+                   className="form-control"
+                   name="city"
+                   placeholder="City e.g. Oakland"
+                   onChange={this.onInputChange}
+                   value={ this.state.city } />
           </div>
           <button type="submit" className="btn btn-primary submit-btn">Submit</button>
         </div>
