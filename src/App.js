5d585f89906d619ea16892a96c0ac3025c188ab0
spragala <spragala@yahoo.com>
2017-06-14 09:39:29 -0700

    Restaurant component added and lined to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Restaurant from './components/Restaurant'
 import $ from 'jquery-ajax';
 
 class App extends Component {
@@ -22,28 +23,7 @@ class App extends Component {
     return (
       <div className="App">
           <Header />
-          <div className="container">
-        <div className="row" data-id="${restaurants._id}">
-          <div className="col-xs-1">
-            <img src="#" className="images" />
-          </div>
-          <div className="col-xs-4">
-            <h2 className="restaurant-name">Restaurant Name</h2>
-            <p className="rating">Rating stars out of 5</p>
-            <p className="address">Address</p>
-            <p className="text-muted"><em>Restaurant Type</em></p>
-          </div>
-          <div className="col-xs-4">
-            <p><strong>Notes:</strong></p>
-            <p className="notes"></p>
-          </div>
-          <div className="col-xs-2">
-            <button type="button" name="button" className="deleteBtn btn btn-default" data-id="${restaurants._id}">
-              <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
-            </button>
-          </div>
-        </div>
-      </div>
+          <Restaurant />
           <Footer />
       </div>
     );
